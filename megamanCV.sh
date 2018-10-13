b0294f461add92ce09b7fc76bd6f3ea96d2f14f6
Marx <costarafael159@gmail.com>
2018-10-13 18:50:29 -0300

    Consertando script tinha esquecido de passar o parametro para o script

--- megamanCV.sh
+++ megamanCV.sh
@@ -2,5 +2,5 @@
 escala=$1
 if [ -z $1 ]; then escala=2; fi
 
-nestopia -d Arquivos/MegaMan3.nes -s $1 &
-python3 megamanCV.py $1
+nestopia -d Arquivos/MegaMan3.nes -s $escala > /dev/null &
+python3 megamanCV.py $escala
