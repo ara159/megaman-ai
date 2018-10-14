82a73754029b798b4c37ab2a3276097cdafbffe1
Marx <costarafael159@gmail.com>
2018-10-14 17:20:55 -0300

    Esqueci de trocar o caminho

--- megamanCV.sh
+++ megamanCV.sh
@@ -2,5 +2,5 @@
 escala=$1
 if [ -z $1 ]; then escala=2; fi
 
-nestopia -d Arquivos/MegaMan3.nes -s $escala > /dev/null &
+nestopia -d arquivos/MegaMan3.nes -s $escala 2> /dev/null &
 python3 megamanCV.py $escala
