4c54b7bd2f5d9dd64dd0a7918a952190b89a21ee
Rafael <costarafael159@gmail.com>
2019-04-21 22:25:18 -0300

    Janela O método janela, diminui a área de ação do metchTemplate Com isso, houve um grande aumento de velociadade de análise do video. De 15m para ~ 3m50s

--- megaman_ai/visao.py
+++ megaman_ai/visao.py
@@ -9,6 +9,7 @@ class MegaMan:
     estado  = None
     direcao = 0
     posicao = None
+    sobra   = 20
     
     def __init__(self, sprites):
         for estado in sprites:
@@ -30,12 +31,26 @@ class MegaMan:
                 for mascara in sprites[estado]["mascaras"]:
                     imagem = cv2.imread(mascara, 0)
                     self.sprites[estado]["mascaras"].append(imagem)
+        self.rotulos = [estado for estado in self.sprites]
+
+    def janela(self, imagem):
+        if self.posicao != None:
+            x, y = self.posicao
+            descy = y + 31 + self.sobra
+            descx = x + 25 + self.sobra
+            if y > self.sobra and x > self.sobra and \
+                    descy < imagem.shape[0] and descx < imagem.shape[1]:
+                return imagem[y-self.sobra:descy, x-self.sobra:descx]
+            else:
+                self.posicao = None
+        return imagem
 
     def atualizar(self, imagem, threashold):
         melhor  = 100
         estado  = None
         posicao = None
         direcao = ""
+        janela = self.janela(imagem)
 
         for _estado in self.sprites:
             todos = self.sprites[_estado]["sprites"]
@@ -51,7 +66,7 @@ class MegaMan:
                         _sprite = cv2.flip(_sprite, 1)
                         _mask   = cv2.flip(_mask, 1)
 
-                    encontrado  = cv2.matchTemplate(imagem, _sprite, cv2.TM_SQDIFF, None, _mask)
+                    encontrado  = cv2.matchTemplate(janela, _sprite, cv2.TM_SQDIFF, None, _mask)
                     status      = cv2.minMaxLoc(encontrado)
 
                     if status[0] < melhor:
@@ -59,15 +74,16 @@ class MegaMan:
                         estado  = _estado
                         direcao = _direcao
                         posicao = status[2]
-
         else:
             if melhor <= threashold:
                 self.estado = [estado, direcao]
-                self.posicao = posicao
+                if self.posicao == None:
+                    self.posicao = posicao
+                else:
+                    self.posicao = (self.posicao[0]-self.sobra+posicao[0], self.posicao[1]-self.sobra+posicao[1]) 
             else:
                 self.estado = [None, None]
                 self.posicao = None
-
         return melhor
     
     def desenhar_infos(self, frame, progresso, qualidade):
