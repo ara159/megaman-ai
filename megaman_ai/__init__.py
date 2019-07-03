dad348aa95160ede8dc8736496821b29564c8545
Rafael Costa <costarafael159@gmail.com>
2019-07-03 01:04:27 -0300

    Retirada temporária do modo jogar

--- megaman_ai/__init__.py
+++ megaman_ai/__init__.py
@@ -1,27 +1,17 @@
-import getopt
+from . import parametros, treinamento, visao
 
-class Config:
-    # Classe de configuração
-    pass
+# def jogar(room,
+#             sequencia  = [],
+#             focar      = True,
+#             foco_tx    = 0.3,
+#             carregar   = False,
+#             escala     = 1,
+#             exibir     = False):
 
-config = Config()
-
-from megaman_ai import emulador, coleta, megaman3
-from server.rafael import *
-import threading
-
-def jogar(room, 
-            sequencia  = [], 
-            focar      = True, 
-            foco_tx    = 0.3, 
-            carregar   = False, 
-            escala     = 1,
-            exibir     = False):
-    
-    emu = emulador.Emulador(room=room)
-    emu.start()
-    print("Emulador iniciado")
-    conectar()
-    print("Conectado ao emulador")
-    print("Fim Thread principal")
-    return emu
+#     emu = emulador.Emulador(room=room)
+#     emu.start()
+#     print("Emulador iniciado")
+#     conectar()
+#     print("Conectado ao emulador")
+#     print("Fim Thread principal")
+#     return emu
